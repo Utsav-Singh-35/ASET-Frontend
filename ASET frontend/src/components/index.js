e9a847d094cc7fd2a3f57f51bfd356d2f165d731
Om Singh <jayom5797@gmail.com>
2025-12-24 14:46:53 +0530

    Delete ASET frontend directory

--- ASET frontend/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Sidebar } from './Sidebar';
-export { default as ChatArea } from './ChatArea';
-export { default as WelcomeScreen } from './WelcomeScreen';
-export { default as MessagesArea } from './MessagesArea';
-export { default as HistoryView } from './HistoryView';
-export { default as UserDropdown } from './UserDropdown';
-export { default as EditChatModal } from './EditChatModal';
